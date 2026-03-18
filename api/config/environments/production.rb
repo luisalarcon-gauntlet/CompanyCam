690ea303c7005ef754a746d26e43ca77728d0b81
Luis Alarcon <luis.alarcon@challenger.gauntletai.com>
2026-03-17 23:22:35 -0500

    fixing issue with backend api deployment

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-  config.active_storage.service = :amazon
+  config.active_storage.service = :production
   config.log_level = :info
   config.log_tags = [:request_id]
   config.action_mailer.perform_caching = false
